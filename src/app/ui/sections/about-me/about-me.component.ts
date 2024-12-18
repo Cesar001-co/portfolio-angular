41f2d9cca5316e75e5f3cc39f82df0594ce2b41f
Cesar Rodriguez <59242247+Cesar001-co@users.noreply.github.com>
2024-12-18 11:00:35 -0500

    themes section1

--- src/app/ui/sections/about-me/about-me.component.ts
+++ src/app/ui/sections/about-me/about-me.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
+  email = "cesarcamilo001@gmail.com";
+  copyMessage: string = '';
 
+  copyEmail() {
+    const input = document.createElement('textarea');
+    input.value = this.email;
+    input.select();
+    document.execCommand('copy');
+    alert("Mail copied to the clipboard.");
+  }
+
+  openPDF() {
+    window.open('assets/cv/CV_Cesar_Rodriguez.pdf', '_blank');
+  }
+
+  openLink(link: string) {
+    window.open(link, '_blank');
+  }
 }
