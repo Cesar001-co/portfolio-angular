73dc3f0c882f8fb8b447b2ffd13d89607ac6fb07
Cesar Rodriguez <59242247+Cesar001-co@users.noreply.github.com>
2024-12-19 23:39:54 -0500

    navbar: zindex fixed, section3: content

---
+++ src/app/domain/models/proyects.interface.ts
@@ -0,0 +1,9 @@
+export interface Proyects {
+    imgUrl: string;
+    name: string;
+    type: string;
+    description: string;
+    techologies?: string[];
+    github?: string;
+    web?: string;
+}
