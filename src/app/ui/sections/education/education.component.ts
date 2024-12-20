aed89e5b340bedb8c3d0eeac7a55cd69af41d5f5
Cesar Rodriguez <59242247+Cesar001-co@users.noreply.github.com>
2024-12-19 21:29:13 -0500

    update: about-me location responsive, section2: icons

--- src/app/ui/sections/education/education.component.ts
+++ src/app/ui/sections/education/education.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Icons_frame, Icons_lang } from '../../../domain/icons/icons';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'education',
@@ -8,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrl: './education.component.scss'
 })
 export class EducationComponent {
+  icons_lang: { name: string; svg: SafeHtml }[] = [];
+  icons_frame:{ name: string; svg: SafeHtml }[] = [];
 
+  constructor(private sanitizer: DomSanitizer) {
+    this.icons_lang = Icons_lang.map(icon => ({
+      ...icon,
+      svg: this.sanitizer.bypassSecurityTrustHtml(icon.svg)
+    }));
+
+    this.icons_frame = Icons_frame.map(icon => ({
+      ...icon,
+      svg: this.sanitizer.bypassSecurityTrustHtml(icon.svg)
+    }));
+  }
 }
