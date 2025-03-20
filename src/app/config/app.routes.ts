4909a42b8e8be8505f257ed082a934eb5e9f006b
Cesar Rodriguez <59242247+Cesar001-co@users.noreply.github.com>
2025-03-20 00:09:31 -0500

    test

--- src/app/config/app.routes.ts
+++ src/app/config/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { MainComponent } from '../ui/pages/main/main.component';
 
 export const routes: Routes = [
-    {path: '', component: MainComponent}
+    {path: '', component: MainComponent},
+    { path: '**', redirectTo: '/es' }
 ];
