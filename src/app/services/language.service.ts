889142840bdb77376e33606df3ed1130603a0c3f
Cesar Rodriguez <59242247+Cesar001-co@users.noreply.github.com>
2025-03-28 19:02:22 -0500

    test v1.8

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -34,9 +34,8 @@ export class LanguageService {
   }
 
   setLanguage(lang: string) {
-    // localStorage.setItem('lang', lang);
-    // this.redirectToLang(lang);
-    this.switchLanguage(lang);
+    localStorage.setItem('lang', lang);
+    this.redirectToLang(lang);
   }
 
   redirectToLang(lang: string) {
@@ -52,11 +51,5 @@ export class LanguageService {
     // // this.router.navigate([`/${lang}`, currentPath]);
     const currentPath = this.router.url.split('/').slice(2).join('/');
     window.location.href = `/${lang}/${currentPath}`;
-  }
-
-  switchLanguage(lang: string) {
-    const currentPath = window.location.pathname;
-    const newPath = currentPath.replace(/^\/(en|es)/, `/${lang}`);
-    window.location.href = newPath;
   }
 }
