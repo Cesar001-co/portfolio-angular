f955e6cac0822ab1d6d6c1d88448d8f444963fe6
Cesar Rodriguez <59242247+Cesar001-co@users.noreply.github.com>
2025-04-07 19:23:04 -0500

    feat: rename MoreProyectsComponent to MoreProjectsComponent and update related routes; add new MoreProjectsComponent files

--- src/app/config/app.routes.ts
+++ src/app/config/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from '../ui/pages/main/main.component';
-import { MoreProyectsComponent } from '../ui/pages/more-proyects/more-proyects.component';
+import { MoreProjectsComponent } from '../ui/pages/more-projects/more-projects.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainComponent },
-    { path: 'more-projects', component: MoreProyectsComponent },
+    { path: 'more-projects', component: MoreProjectsComponent },
     { path: '**', redirectTo: '/home' }
 ];
---
+++ src/app/ui/pages/more-projects/more-projects.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-more-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './more-projects.component.html',
+  styleUrl: './more-projects.component.scss'
+})
+export class MoreProjectsComponent {
+
+}
