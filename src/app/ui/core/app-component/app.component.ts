c6ef548c6150eb2f725b48668f0b623cb64d1fba
Cesar Rodriguez <59242247+Cesar001-co@users.noreply.github.com>
2026-02-24 15:24:23 -0500

    feat: update version to 2.1.0, enhance AppComponent with title management on focus/blur, and adjust hamburger menu background color

--- src/app/ui/core/app-component/app.component.ts
+++ src/app/ui/core/app-component/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SettingsButtonComponent } from '../../design/settings-button/settings-button.component';
+import { Title } from '@angular/platform-browser';
+import { LanguageService } from '../../../services/language.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,22 @@ import { SettingsButtonComponent } from '../../design/settings-button/settings-b
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  private readonly previusTitle: string = 'Cesar.dev';
+  
+  constructor (
+    private titleService: Title, 
+    private languageServie: LanguageService
+  ) {}
+
+  @HostListener('window:blur')
+  onBlur() {
+    this.titleService.setTitle(this.languageServie.getLanguage() === 'es' ? `¡No te vayas! ¡Vuelve! 😱` : `Don't go! Come back! 😱`);
+  }
+
+  @HostListener('window:focus')
+  onFocus() {
+    this.titleService.setTitle(this.previusTitle);
+  }
+  
 }
